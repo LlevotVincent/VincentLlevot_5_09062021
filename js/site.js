5e6d5d1d3b01336e806643d5bd116b2d81edc2aa
LlevotVincent <vincent.llevot@gmail.com>
2021-06-15 16:20:51 +0200

    creation du tableau ingredient

--- js/site.js
+++ js/site.js
@@ -1,28 +1,27 @@
 
-const searchBar = document.getElementById("searchbar__text");
-
-searchBar.addEventListener("keyup",ingredientArray)
-const inputSearchBar = searchBar.value;
-
-
-
-
 //creer un tableau des ingrédients
+const ingredientArray = [];
+createIngredientArray(ingredientArray);
 
-function ingredientArray(){
+function createIngredientArray (ingredientList){
     for (var i = 0; i < recipes.length; i++) {
-        let ingredients = (recipes[i].ingredients);
+        let ingredients = recipes[i].ingredients;
         for (var j = 0; j < ingredients.length; j++){
-            let ingredient = (ingredients[j].ingredient);
-            if (ingredient.includes(inputSearchBar)){
-                console.log(ingredient)
+            let ingredient = ingredients[j].ingredient;
+            if (!ingredientList.includes(ingredient)){
+                ingredientList.push(ingredient);
             }
         }
     }
 }
 
-
-
-
+//trie le tableau ingredients suivant l'input de la barre de recherche Ne fonctionne pas
+const searchBar = document.getElementById("searchbar__text");
+searchBar.addEventListener("keyup", function(){
+    console.table(ingredientArray);
+    const inputSearchBar = searchbar__text.value;
+    const ingredientfilter = ingredientArray.filter(item => item.value.includes(inputSearchBar));
+    console.table(ingredientfilter)
+});
 
 
